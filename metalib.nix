9f3a512dc81994949b26f675df4047ddeb9c8fd5
John Wiegley <johnw@newartisans.com>
2017-07-20 02:48:30 +0000

    A fix to the metalib installation

--- metalib.nix
+++ metalib.nix
@@ -20,8 +20,7 @@ stdenv.mkDerivation {
   '';
 
   installPhase = ''
-    mkdir -p $out/lib/coq/${coq.coq-version}/Metalib
-    (cd Metalib; make -f CoqSrc.mk COQLIB=$out/lib/coq/${coq.coq-version}/Metalib install)
+    (cd Metalib; make -f CoqSrc.mk DSTROOT=/ COQLIB=$out/lib/coq/${coq.coq-version}/ install)
   '';
 
   meta = with stdenv.lib; {
